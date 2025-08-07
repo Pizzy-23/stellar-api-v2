ccf74d83831c4ade65a3ee6864a39a7146782466
Pizzy-23 <parzival1.2323@gmail.com>
2025-08-07 01:20:25 -0300

    fix for url db

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,18 +2,34 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  synchronize: false,
-  entities: ['src/**/.entity.ts'],
-  migrations: ['src/migrations/.ts'],
-  migrationsTableName: 'migrations',
-};
+function getDataSourceOptions(): DataSourceOptions {
+  // Se existir DATABASE_URL no .env, usa ela
+  if (process.env.DATABASE_URL) {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: false,
+      entities: ['src/**/*.entity.ts'],
+      migrations: ['src/migrations/*.ts'],
+      migrationsTableName: 'migrations',
+    };
+  }
+
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: false,
+    entities: ['src/**/*.entity.ts'],
+    migrations: ['src/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+  };
+}
+
+export const dataSourceOptions: DataSourceOptions = getDataSourceOptions();
 
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
